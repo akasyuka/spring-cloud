d6f15d6c751ae96923ac80a254be917ac525688a
Александр Касюк <aleksandrkasyuk@gmail.com>
2022-11-04 19:36:48 +0300

    Postgres error

---
+++ customer/src/main/java/org/example/customer/Customer.java
@@ -0,0 +1,32 @@
+package org.example.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+    public Integer id;
+    public String firstName;
+    public String lastName;
+    public String email;
+
+
+}
---
+++ customer/src/main/java/org/example/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,6 @@
+package org.example.customer;
+
+public record CustomerRegistrationRequest (String firstName,
+                                           String lastName,
+                                           String email){
+}
